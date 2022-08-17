463b21b72f22cf7158dbebc7aec2f2496ac6d379
VictorLino <victor.lino@audatex.com>
2022-08-17 18:35:28 -0500

    Initial commit with Classes and Methods projects for its concepts

---
+++ Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
---
+++ Methods/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Methods
+{
+    partial class Program
+    {
+        public class Calculator
+        {
+            public int Add(params int[] numbers)
+            {
+                return numbers.Sum();
+            }
+        }
+    }
+}
