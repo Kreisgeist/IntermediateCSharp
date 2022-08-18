4402803d5987d77875977c18b6147c1657a22735
Gwynrael <victor.lino.dev@gmail.com>
2022-08-17 20:46:52 -0500

    Access Modifiers project added

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime _birthday;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthday = birthdate;
+        }
+        public DateTime GetBirthdate()
+        {
+            return _birthday;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
