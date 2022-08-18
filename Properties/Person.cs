03f7d34b8aa7000c46f798e1e43bf8b44d7030db
VictorLino <victor.lino@audatex.com>
2022-08-18 09:55:21 -0500

    Project for Properties Added

---
+++ Properties/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace Properties
+{
+    internal partial class Program
+    {
+        public class Person
+        {
+            public DateTime Birthdate { get; private set; }
+            public string Name { get; set; }
+            public string Username { get; set; }
+
+            public Person(DateTime birthdate)
+            {
+                Birthdate = birthdate;
+            }
+
+            public int Age
+            {
+                get
+                {
+                    var timeSpan = DateTime.Today - Birthdate;
+                    var years = timeSpan.Days / 365;
+
+                    return years;
+                }
+            }
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Properties
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
