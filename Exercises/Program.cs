77a541a8f97a241389f3849aaba1da8b5f5ac649
VictorLino <victor.lino@audatex.com>
2022-08-18 18:03:43 -0500

    Stopwatch exercise done.

---
+++ Exercises/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            activateStopwatch();
+        }
+
+        public static void activateStopwatch()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            ConsoleKey decision;
+            bool isPainted = false;
+            bool isStarted = false;
+
+            do
+            {
+                if (!isPainted)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Start stopwatch (Enter) - Stop watch (ESC) - Exit (Space)");
+                    isPainted = true;
+                }
+
+                decision = Console.ReadKey().Key;
+
+                if (!isStarted && decision == ConsoleKey.Enter)
+                {
+                    isStarted = true;
+                    Console.WriteLine(stopwatch.Start() + "Press stop when ready");
+                }
+                else if (isStarted && decision == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Time elapsed: {0}; Press 1 to continue", stopwatch.Stop());
+                    isPainted = false;
+                    isStarted = false;
+                    do
+                    {
+                        decision = Console.ReadKey().Key;
+                    } while (decision != ConsoleKey.D1);
+                } 
+
+            } while (decision != ConsoleKey.Spacebar);
+
+        }
+
+    }
+}
---
+++ Exercises/Stopwatch.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercises
+{
+    public class Stopwatch
+    {
+        private DateTime _start;
+        private bool _isStarted = false;
+
+        public string Start()
+        {
+            if(!_isStarted)
+            {
+                this._start = DateTime.Now;
+                this._isStarted = true;
+
+                return "Watch Started. ";
+            }
+            else
+            {
+                return "Already Started. ";
+            }
+
+        }
+        public TimeSpan Stop()
+        {
+            this._isStarted = false;
+            return DateTime.Now - this._start;
+        }
+    }
+}
