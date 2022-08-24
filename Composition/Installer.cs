71bd723ee7b3aef86ae2ad37f2a2bacb09b7e413
VictorLino <victor.lino@audatex.com>
2022-08-23 19:29:19 -0500

    Composition and Inheritance projects added

---
+++ Composition/Installer.cs
@@ -0,0 +1,19 @@
+﻿namespace Composition
+{
+    internal partial class Program
+    {
+        public class Installer
+        {
+            private readonly Logger _logger;
+
+            public Installer(Logger logger)
+            {
+                this._logger = logger;
+            }
+            public void Install()
+            {
+                _logger.Log("We are installing the application.");
+            }
+        }
+    }
+}
---
+++ Composition/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Composition
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
+        }
+    }
+}
