2fffb96d4789808700911f932a72df0264786bae
Victor Lino <victor.lino@jato.com>
2022-10-10 16:41:22 -0500

    Testability class

---
+++ Testability/OrderProccesor.cs
@@ -0,0 +1,24 @@
+﻿namespace Testability
+{
+    public class OrderProccesor
+    {
+        private readonly IShippingCalculator _shippingCalculator;
+
+        public OrderProccesor(IShippingCalculator shippingCalculator)
+        {
+            _shippingCalculator = shippingCalculator;
+        }
+
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order is already processed");
+
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1)
+            };
+        }
+    }
+}
